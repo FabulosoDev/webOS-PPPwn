4b7f5a9a58c44dac8fb0067ce989ae8a61a68422
Fabuloso Dev <113933725+FabulosoDev@users.noreply.github.com>
2024-05-24 20:50:28 +0200

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,15 +1,13 @@
 function terminalLog(content) {
-    var wasReplaceChild = false;
     function addChild(line) {
         var terminal = document.querySelector('#terminal');
         var newInput = document.createElement('span');
         newInput.setAttribute('class', 'terminal-line');
         newInput.textContent = line;
-        if (!wasReplaceChild && (!terminal.firstChild || line.slice(-1) == '\n')) {
+        if (!terminal.firstChild || line.slice(-1) == '\n') {
             terminal.appendChild(newInput);
         } else {
             terminal.replaceChild(newInput, terminal.lastChild);
-            was Replace Child = true;
         }
         terminal.scrollTop = terminal.scrollHeight
     }
