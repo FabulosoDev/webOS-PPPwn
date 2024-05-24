820470bedb6ffbcb8d7d2febeadf64940044158b
Fabuloso Dev <113933725+FabulosoDev@users.noreply.github.com>
2024-05-24 19:26:57 +0200

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -25,14 +25,42 @@ function terminalLog(content) {
 }
 
 function installPppwn() {
-    console.log('echo "Install button clicked"');
-    terminalLog('echo "Install button clicked"');
+    var installCmd = 
+        `mkdir -p /media/internal/downloads/webOS-PPPwn &&
+         curl -fsSL -o /media/internal/downloads/webOS-PPPwn/pppwn https://github.com/FabulosoDev/PPLGPwn/raw/main/pppwn_armv7 &&
+         curl -fsSL -o /media/internal/downloads/webOS-PPPwn/stage1.bin https://github.com/FabulosoDev/PPLGPwn/raw/main/stage1/1100/stage1.bin
+         curl -fsSL -o /media/internal/downloads/webOS-PPPwn/stage2.bin https://github.com/FabulosoDev/PPLGPwn/raw/main/stage2/1100/stage2.bin`;
+
+    webOS.service.request("luna://org.webosbrew.hbchannel.service", {
+        method: "spawn",
+        parameters: {"command": installCmd},
+        onSuccess: function (response) {
+            switch(response.event) {
+                case "stdoutData":
+                    terminalLog(response.stdoutString);
+                    console.log(response.stdoutString);
+                    break;
+            }            
+        },
+        onFailure: function (error) {
+            terminalLog(error);
+            console.log(error);
+            return;
+        },
+        subscribe: true,
+        resubscribe: true
+    });
 }
 
 function runPppwn() {
+    var runCmd = 
+        `cd /media/internal/downloads/webOS-PPPwn &&
+         chmod +x ./pppwn &&
+         ./pppwn -i eth0 --fw 1100 -s1 stage1.bin -s2 stage2.bin -t 60`;
+
     webOS.service.request("luna://org.webosbrew.hbchannel.service", {
         method: "spawn",
-        parameters: {"command": "cd /media/internal/downloads/PPLGPwn && chmod +x ./run.sh && ./run.sh"},
+        parameters: {"command": runCmd},
         onSuccess: function (response) {
             switch(response.event) {
                 case "stdoutData":
